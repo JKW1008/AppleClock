67f39c50c8ec825719d21f22c35fa4e743a7f0e7
JKW1008 <todendvv91@gmail.com>
2024-04-09 01:56:08 +0900

    step 8

--- AppleClock/AppleClock/Cell/WorldClockTableViewCell.swift
+++ AppleClock/AppleClock/Cell/WorldClockTableViewCell.swift
@@ -10,16 +10,17 @@ import UIKit
 class WorldClockTableViewCell: UITableViewCell {
     
     @IBOutlet weak var timeOffsetLabel: UILabel!
-    
     @IBOutlet weak var timeZoneLabel: UILabel!
-    
     @IBOutlet weak var timePeriodLabel: UILabel!
-
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet var spacingContraint: NSLayoutConstraint!
+    @IBOutlet weak var timeLabelTrailingConstraint: NSLayoutConstraint!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        timeLabel.backgroundColor = .systemBackground
+        timePeriodLabel.backgroundColor = .systemBackground
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -28,4 +29,20 @@ class WorldClockTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    override func setEditing(_ editing: Bool, animated: Bool) {
+        super.setEditing(editing, animated: animated)
+        
+        if (self.superview as? UITableView) == nil {
+            return
+        }
+        
+        timePeriodLabel.isHidden = editing
+        timeLabel.isHidden = editing
+        spacingContraint.isActive = !editing
+        timeLabelTrailingConstraint.constant = editing ? -40 : 0
+        
+        UIView.animate(withDuration: 0.3) {
+            self.layoutIfNeeded()
+        }
+    }
 }
