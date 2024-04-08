8af84d8131eefc350024c270ba0fcfe6fb7b4192
JKW1008 <todendvv91@gmail.com>
2024-04-09 00:43:21 +0900

    step 5

--- AppleClock/AppleClock/CitySelectionViewController.swift
+++ AppleClock/AppleClock/CitySelectionViewController.swift
@@ -14,7 +14,7 @@ struct Item {
 
 struct Section {
     let title: String
-    let times: [Item]
+    let items: [Item]
 }
 
 class CitySelectionViewController: UIViewController {
@@ -38,10 +38,58 @@ class CitySelectionViewController: UIViewController {
             
             dict[key] = timeZoneList
         }
+        
+        for (key, value) in dict {
+            let items = value.sorted {
+                guard let lhs = $0.city, let rhs = $1.city else { return false }
+                return lhs < rhs
+            }.map {
+                Item(title: $0.city ?? $0.identifier, timeZone:  $0)
+            }
+            
+            let section = Section(title: key, items: items)
+            list.append(section)
+        }
+        
+        list.sort { $0.title < $1.title }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupList()
+        cityTableView.reloadData()
+    }
+}
 
+extension CitySelectionViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return list.count
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list[section].items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cityCell", for: indexPath)
+        
+        let target = list[indexPath.section].items[indexPath.row]
+        cell.textLabel?.text = target.title
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        return list[section].title
+    }
+    
+    func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        let consonantsAndAlphapets: [String] = ["ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        
+        return consonantsAndAlphapets
+    }
+    
+    func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+        return list.firstIndex(where: { $0.title.uppercased() == title.uppercased() }) ?? 0
     }
 }
